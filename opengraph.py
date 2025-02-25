bd0d2f34529c24527900c0293805ff72e7117a9a
Abdulmajeed Alrashidi <alrashabdulmajeed@gmail.com>
2025-02-25 10:58:21 +0300

    Initiate challenge

---
+++ opengraph.py
@@ -0,0 +1,18 @@
+# pylint: disable=no-value-for-parameter
+"""
+Client of the Wagon OpenGraph API
+"""
+
+import requests
+
+def fetch_metadata(url):
+    """
+    Returns the "data" dictionary of OpenGraph metadata found in HTML of given url
+    """
+    pass  # YOUR CODE HERE
+
+# # To manually test, uncomment the following lines and run `python opengraph.py`:
+# if __name__ == "__main__":
+#     import pprint
+#     pp = pprint.PrettyPrinter(indent=4)
+#     pp.pprint(fetch_metadata("https://www.github.com"))
